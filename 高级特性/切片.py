72ff764c5eb74160ab140047d607944e3ba1d765
wang-jun@bmsoft.com.cn <wangjun@681>
2022-07-28 10:41:11 +0800

    增加切片迭代，函数

---
+++ 高级特性/切片.py
@@ -0,0 +1,12 @@
+# encoding：utf-8
+
+L = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff']
+
+# 左闭右开
+# 取前三个元素
+print(L[:3])
+# 取最后一个元素
+print(L[-1])
+#取最后两个
+print(L[-2:])
+print(L[-3:-2])
---
+++ 高级特性/迭代.py
@@ -0,0 +1,9 @@
+# encoding：utf-8
+
+def findMinAndMax(L):
+    return max(L), min(L)
+
+
+L = [48, 98, 156, 35, -9, 48]
+value = findMinAndMax(L)
+print(value)
